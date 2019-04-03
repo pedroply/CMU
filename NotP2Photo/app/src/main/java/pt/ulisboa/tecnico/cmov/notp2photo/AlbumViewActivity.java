13a0d2e0c4be0176294c79842f9fbb507cda0657
pedro medeiros <pedromedeiros@outlook.com>
2019-04-03 15:29:30 +0100

    added more interactions between app and server, you can now create albuns view them and add links to them

---
+++ NotP2Photo/app/src/main/java/pt/ulisboa/tecnico/cmov/notp2photo/AlbumViewActivity.java
@@ -0,0 +1,134 @@
+package pt.ulisboa.tecnico.cmov.notp2photo;
+
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class AlbumViewActivity extends AppCompatActivity {
+
+    private static String TAG = "AlbumViewActivity";
+    private String user;
+    private String token;
+    private String album;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_view);
+
+        token = getIntent().getStringExtra("token");
+        user = getIntent().getStringExtra("user");
+        album = getIntent().getStringExtra("album");
+
+        TextView albumTextView = findViewById(R.id.albumTextView);
+        albumTextView.setText(album);
+
+        findViewById(R.id.refreshLinksButton).performClick();
+
+        System.out.println("User: " + user + " token: " + token + " album: " + album);
+    }
+
+    public void postLink(View v) {
+        // /postLink?name=pedro&token=ObVNGg==&album=test
+
+        EditText linkTextInput = findViewById(R.id.linkInputText);
+        String link = linkTextInput.getText().toString();
+
+        String url = "http://" + WebInterface.IP + "/postLink?name="+user+"&token="+token+"&album="+album;
+        Log.d(TAG, "URL: " + url);
+        System.out.println(url);
+
+        new postLinkTask().execute(url, link);
+    }
+
+    public void refreshLinks(View v) {
+        // /retriveAlbum?name=pedro&token=ObVNGg==&album=test
+
+        String url = "http://" + WebInterface.IP + "/retriveAlbum?name="+user+"&token="+token+"&album="+album;
+        Log.d(TAG, "URL: " + url);
+        System.out.println(url);
+
+        new refreshLinksTask().execute(url);
+    }
+
+    class postLinkTask extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... urls) {
+
+            String response = WebInterface.post(urls[0], urls[1]);
+            Log.i(TAG, "Response: " + response.toString());
+            System.out.println(response.toString());
+            return response.toString();
+        }
+
+        protected void onPostExecute(String response) {
+            System.out.println("On main: " + response);
+            try {
+                JSONObject mainObject = new JSONObject(response);
+                if(mainObject.has("response") && mainObject.getString("response").equals(WebInterface.responseOK)){
+                    Toast toast = Toast.makeText(getApplicationContext(), "New Link Posted!", Toast.LENGTH_SHORT);
+                    toast.show();
+                    findViewById(R.id.refreshLinksButton).performClick();
+                }
+                else{
+                    Toast toast = Toast.makeText(getApplicationContext(), "Link Error!", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Toast toast = Toast.makeText(getApplicationContext(), "Link Error!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+        }
+    }
+
+    class refreshLinksTask extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... urls) {
+
+            String response = WebInterface.get(urls[0]);
+            Log.i(TAG, "Response: " + response.toString());
+            System.out.println(response.toString());
+            return response.toString();
+        }
+
+        protected void onPostExecute(String response) {
+            System.out.println("On main: " + response);
+            LinearLayout linkContainer = findViewById(R.id.linkContainer);
+            linkContainer.removeAllViews();
+            try {
+                JSONObject mainObject = new JSONObject(response);
+                JSONArray linksArray = mainObject.getJSONArray("links");
+                for(int i = 0; i<linksArray.length(); i++){
+                    TextView tmpLinkView = new TextView(getBaseContext());
+                    tmpLinkView.setTextColor(Color.parseColor("#0061ff"));
+                    tmpLinkView.setTextSize(15);
+                    tmpLinkView.setText(linksArray.getString(i));
+                    linkContainer.addView(tmpLinkView);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Toast toast = Toast.makeText(getApplicationContext(), "Album Error!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+        }
+    }
+
+}
