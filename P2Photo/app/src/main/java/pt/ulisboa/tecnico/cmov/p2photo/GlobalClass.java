2b2754f34ad60f0bd437689942e77038c2e17543
FilipaSRPedrosa <filipa.s.r.pedrosa@tecnico.ulisboa.pt>
2019-04-22 14:31:27 +0100

    Added MainActivity + HomeActivity base structure

---
+++ P2Photo/app/src/main/java/pt/ulisboa/tecnico/cmov/p2photo/GlobalClass.java
@@ -0,0 +1,93 @@
+package pt.ulisboa.tecnico.cmov.p2photo;
+
+import android.app.ActivityManager;
+import android.app.Application;
+import android.graphics.Bitmap;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class GlobalClass extends Application {
+
+    private UserInfo user;
+    private Bitmap selectedPhoto, servicePhoto;
+    private ArrayList<String> eventLog = new ArrayList<String>();
+
+    public void createUser(String userName, String loginToken){
+        user = new UserInfo(userName, loginToken);
+    }
+
+    public String getUserName(){
+        return user.getUserName();
+    }
+
+    public String getUserLoginToken(){
+        return user.getLoginToken();
+    }
+
+    public boolean albumListEmpty(){
+        return user.albumListEmpty();
+    }
+
+    public void addUserAlbums(ArrayList<String> albumsList){
+        for(String album : albumsList){
+            if(!user.containsAlbum(album)){
+                user.addNewAlbum(album);
+            }
+        }
+    }
+
+    public void addNewAlbum(String albumName){
+        user.addNewAlbum(albumName);
+    }
+
+    public ArrayList<String> getAlbumList(){
+        return user.getAlbumList();
+    }
+
+    public boolean albumPhotosIsEmpty(String album){
+        return user.albumPhotosIsEmpty(album);
+    }
+
+    public void addPhotosToAlbum(String album, TreeMap<String, Bitmap> photos){
+        user.addPhotosToAlbum(album, photos);
+    }
+
+    public ArrayList<Bitmap> getAlbumPhotoList(String album){
+        return user.getPhotosList(album);
+    }
+
+    public void setSelectedPhoto(Bitmap bitmap){
+        selectedPhoto = bitmap;
+    }
+
+    public Bitmap getSelectedPhoto(){
+        return selectedPhoto;
+    }
+
+    public void setServicePhoto(Bitmap bitmap){
+        servicePhoto = bitmap;
+    }
+
+    public Bitmap getServicePhoto(){
+        return servicePhoto;
+    }
+
+    public void addPhotoToAlbum(String albumName, Bitmap photo, String link){
+        user.addPhotoToAlbum(albumName, photo, link);
+    }
+
+    public boolean containsPhoto(String album, String link){
+        return user.containsPhoto(album, link);
+    }
+
+    public void addEvent(String event){
+        eventLog.add(event);
+    }
+
+    public ArrayList<String> getLogEvent(){
+        return eventLog;
+    }
+
+}
---
+++ P2Photo/app/src/main/java/pt/ulisboa/tecnico/cmov/p2photo/UserInfo.java
@@ -0,0 +1,69 @@
+package pt.ulisboa.tecnico.cmov.p2photo;
+
+import android.graphics.Bitmap;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class UserInfo {
+    private String loginToken;
+    private String userName;
+    private TreeMap<String, TreeMap<String, Bitmap>> albums = new TreeMap<String, TreeMap<String, Bitmap>>();
+
+    public UserInfo(String user, String loginToken){
+        userName = user;
+        this.loginToken = loginToken;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getLoginToken(){
+        return loginToken;
+    }
+
+    public boolean albumListEmpty(){
+        return albums.isEmpty();
+    }
+
+    public synchronized void addNewAlbum(String albumName){
+        albums.put(albumName, new TreeMap<String, Bitmap>());
+    }
+
+    public synchronized ArrayList<String> getAlbumList(){
+        return new ArrayList<String>(albums.keySet());
+    }
+
+    public synchronized boolean albumPhotosIsEmpty(String albumName){
+        TreeMap<String,Bitmap> photos = albums.get(albumName);
+        return photos.isEmpty();
+    }
+
+    public synchronized void addPhotosToAlbum(String albumName, TreeMap<String, Bitmap> photos){
+        albums.put(albumName, photos);
+    }
+
+    public synchronized void addPhotoToAlbum(String albumName, Bitmap photo, String url){
+        TreeMap<String,Bitmap> photos = albums.get(albumName);
+        photos.put(url, photo);
+    }
+
+    public synchronized ArrayList<Bitmap> getPhotosList(String albumName){
+        TreeMap<String, Bitmap> photos =  albums.get(albumName);
+        return new ArrayList<Bitmap>(photos.values());
+    }
+
+    public synchronized boolean containsPhoto(String albumName, String link){
+        TreeMap<String, Bitmap> photos =  albums.get(albumName);
+        return photos.containsKey(link);
+    }
+
+    public synchronized boolean containsAlbum(String albumName){
+        return albums.containsKey(albumName);
+    }
+
+
+}
